587dca5c7c96e0222126b6c2f95cc158cb982a84
Korku02 <korkudeepak@gmail.com>
2017-06-28 01:55:06 +0530

    sign page created

--- dist/js/cap.js
+++ dist/js/cap.js
@@ -1,61 +1,49 @@
-app.controller('MainCtrl', function($scope, $mdToast, $document, $http, $location) {
+app.controller('MainCtrl', function($scope, $mdToast, $document, $http, $location,Auth) {
 
-  // $scope.isPath= function(viewLocation) {
-  //   return viewLocation === $location.path();
-  // };
-  // $scope.signUp=function (user) {
-  //   $http({
-  //     url:URL_PREFIX+"api/register/",
-  //     method:"POST",
-  //     headers:{
-  //       'Content-Type': 'application/json; charset=UTF-8'
-  //     },
-  //     data:{
-  //       'user_name':user.username,
-  //       'user_id':user.userid.toUpperCase(),
-  //       'user_hostel':user.hostel,
-  //       'email':user.email,
-  //       'password':user.password
-  //     }
-  //   }).then(function sucessCallback(response) {
-  //     if (response.status===200){
-  //       $location.path("/login");
-  //       $mdToast.show(
-  //         $mdToast.simple()
-  //         .textContent('User created sucessfully!')
-  //         .position('bottom right')
-  //         .hideDelay(3000)
-  //       );
-  //     }
-  //   }, function errorCallback(error) {
-  //     if (error.status===302){
-  //       $mdToast.show(
-  //         $mdToast.simple()
-  //         .textContent('Something went wrong, Please try again!')
-  //         .position('bottom right')
-  //         .hideDelay(3000)
-  //       );
-  //     }
-  //   });
-  // };
-  // $scope.logInUser=function (user) {
-  //   if (user===undefined) {
-  //     $mdToast.show(
-  //       $mdToast.simple()
-  //       .textContent('Please check your input field')
-  //       .position('bottom right')
-  //       .hideDelay(3000)
-  //     );
-  //   }
-  //   Auth.login(user).then(function(response) {
-  //     $scope.userDetails = response;
-  //     $location.path("/dashboard");
-  //     $mdToast.show(
-  //       $mdToast.simple()
-  //       .textContent('User sucessfully logged in!')
-  //       .position('bottom right')
-  //       .hideDelay(3000)
-  //     );
-  //   });
-  // };
+  $scope.isPath= function(viewLocation) {
+    return viewLocation === $location.path();
+  };
+  $scope.signUp=function (user) {
+    console.log('bhai');
+    $http({
+      url:URL_PREFIX+"api/signup/",
+      method:"POST",
+      headers:{
+        'Content-Type': 'text/html; charset=utf-8'
+      },
+      data:{
+        'email':user.email,
+        'password':user.password
+
+      }
+    }).then(function sucessCallback(response) {
+
+        console.log(response);
+        // $location.path("/login");
+
+    }, function errorCallback(error) {
+      console.log(error);
+    });
+  };
+  $scope.logInUser=function (user) {
+    if (user===undefined) {
+      $mdToast.show(
+        $mdToast.simple()
+        .textContent('Please check your input field')
+        .position('bottom right')
+        .hideDelay(3000)
+      );
+    }
+    Auth.login(user).then(function(response) {
+      // $scope.userDetails = response;
+      // $location.path("/dashboard");
+      console.log(response);
+      $mdToast.show(
+        $mdToast.simple()
+        .textContent('User sucessfully logged in!')
+        .position('bottom right')
+        .hideDelay(3000)
+      );
+    });
+  };
 });
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var app = angular.module('app', ['ngMaterial','ngAnimate','ngRoute']);
 
-var URL_PREFIX = 'http://10.17.5.66:8080/';
+var URL_PREFIX = 'http://10.194.31.195:8080/';
 var CLIENT_ID='usOAFye6wnoJAKX0BWk0EYZshsWO2LZY5zfHM4tb';
 var CLIENT_SECRET='M5qMOfXMWwTVKpHVQt20QVnGFqowylJrCuUxDiuTzkpwaWhDnIv22jNLjOgcNsEeSVH3L3Hunh0GUgULQXKCQYfibqvU4snMbmoMdKvoXFFBv7UcjjmBLpWnBQaWdW8J';
 //
@@ -11,118 +11,100 @@ var CLIENT_SECRET='M5qMOfXMWwTVKpHVQt20QVnGFqowylJrCuUxDiuTzkpwaWhDnIv22jNLjOgcN
 //   $httpProvider.defaults.headers.patch = {};
 // });
 //
-// app.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
-//   $routeProvider.when("/", {
-//     controller: "MainCtrl",
-//     templateUrl: "templates/home.html"
-//   }).when("/login", {
-//     controller: "MainCtrl",
-//     templateUrl: "templates/login.html"
-//   }).when("/signup", {
-//     controller: "MainCtrl",
-//     templateUrl: "templates/signup.html"
-//   }).when("/dashboard", {
-//     controller: "DashCtrl",
-//     templateUrl: "templates/dashboard.html",
-//     resolve: {
-//         auth: function ($q, Auth) {
-//             var userDetails = Auth.getuserDetails();
-//             if (userDetails) {
-//                 return $q.when(userDetails);
-//             } else {
-//                 return $q.reject({ authenticated: false });
-//             }
-//         }
-//     }
-//   }).otherwise({
-//     controller: "MainCtrl",
-//     templateUrl: "templates/error.html"
-//   });
-// }]);
-// app.run(["$rootScope", "$location", function ($rootScope, $location) {
-//     $rootScope.$on("$routeChangeSuccess", function (userDetails) {
-//         // console.log(userDetails);
-//     });
-//     $rootScope.$on("$routeChangeError", function (event, current, previous, eventObj) {
-//         if (eventObj.authenticated === false) {
-//             $location.path("/");
-//         }
-//     });
-// }]);
-// app.factory("Auth", ["$http","$q","$window",function ($http, $q, $window) {
-//     var userDetails;
-//     function login(user) {
-//         var url=URL_PREFIX+'login/';
-//         var deferred = $q.defer();
-//         $http({
-//              method: "POST",
-//              transformRequest: function(obj) {
-//                 var str = [];
-//                 for(var p in obj)
-//                 str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-//                 return str.join("&");
-//              },
-//              data: {
-//                 'email':user.email,
-//                 'password':user.password,
-//                 'client_id': CLIENT_ID,
-//                 'client_secret': CLIENT_SECRET,
-//                 'grant_type': 'client_credentials'
-//              },
-//              headers: {
-//                 'Content-Type': 'application/x-www-form-urlencoded'
-//               },
-//              url: url
-//            }).then(function successCallback(response) {
-//              console.log(response);
-//              userDetails = {
-//                  accessToken: response.data.access_token,
-//                  hostel:response.data.hostel,
-//                  name:response.data.name,
-//                  email: response.data.email,
-//                  id:response.data.id,
-//                  role:response.data.role
-//              };
-//              $window.localStorage.userDetails = JSON.stringify(userDetails);
-//              deferred.resolve(userDetails);
-//            }, function errorCallback(error) {
-//              deferred.reject(error);
-//          });
-//          return deferred.promise;
-//     };
-//
-//     function logout() {
-//         var deferred = $q.defer();
-//         $http({
-//             method: "POST",
-//             url: URL_PREFIX+"logout/",
-//             headers: {
-//               'Content-Type': 'application/json; charset=UTF-8',
-//               'Authorization':'Bearer '+userDetails.accessToken
-//             }
-//         }).then(function (result) {
-//             // console.log(result);
-//             userDetails = null;
-//             $window.localStorage.userDetails = null;
-//             deferred.resolve(result);
-//         }, function (error) {
-//             deferred.reject(error);
-//         });
-//         return deferred.promise;
-//     };
-//
-//     function getuserDetails() {
-//         return userDetails;
-//     }
-//     function init() {
-//         if ($window.localStorage.userDetails) {
-//             userDetails = JSON.parse($window.localStorage.userDetails);
-//         }
-//     }
-//     init();
-//     return {
-//         login: login,
-//         logout: logout,
-//         getuserDetails: getuserDetails
-//     };
-// }]);
+app.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
+  $routeProvider.when("/", {
+    controller: "MainCtrl",
+    templateUrl: "templates/signup.html"
+  }).when("/login", {
+    controller: "MainCtrl",
+    templateUrl: "templates/login.html"
+  }).otherwise({
+    controller: "MainCtrl",
+    templateUrl: "templates/error.html"
+  });
+}]);
+app.run(["$rootScope", "$location", function ($rootScope, $location) {
+    $rootScope.$on("$routeChangeSuccess", function (userDetails) {
+        // console.log(userDetails);
+    });
+    $rootScope.$on("$routeChangeError", function (event, current, previous, eventObj) {
+        if (eventObj.authenticated === false) {
+            $location.path("/");
+        }
+    });
+}]);
+app.factory("Auth", ["$http","$q","$window",function ($http, $q, $window) {
+    var userDetails;
+    function login(user) {
+        var url=URL_PREFIX+'api/login/';
+        var deferred = $q.defer();
+        $http({
+             method: "POST",
+             transformRequest: function(obj) {
+                var str = [];
+                for(var p in obj)
+                str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                return str.join("&");
+             },
+             data: {
+                'email':user.email,
+                'password':user.password
+
+             },
+             headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+              },
+             url: url
+           }).then(function successCallback(response) {
+             console.log(response);
+            //  userDetails = {
+            //      accessToken: response.data.access_token,
+            //      hostel:response.data.hostel,
+            //      name:response.data.name,
+            //      email: response.data.email,
+            //      id:response.data.id,
+            //      role:response.data.role
+            //  };
+            //  $window.localStorage.userDetails = JSON.stringify(userDetails);
+            //  deferred.resolve(userDetails);
+           }, function errorCallback(error) {
+             deferred.reject(error);
+         });
+         return deferred.promise;
+    };
+
+    function logout() {
+        var deferred = $q.defer();
+        $http({
+            method: "POST",
+            url: URL_PREFIX+"logout/",
+            headers: {
+              'Content-Type': 'application/json; charset=UTF-8',
+              'Authorization':'Bearer '+userDetails.accessToken
+            }
+        }).then(function (result) {
+            // console.log(result);
+            userDetails = null;
+            $window.localStorage.userDetails = null;
+            deferred.resolve(result);
+        }, function (error) {
+            deferred.reject(error);
+        });
+        return deferred.promise;
+    };
+
+    function getuserDetails() {
+        return userDetails;
+    }
+    function init() {
+        if ($window.localStorage.userDetails) {
+            userDetails = JSON.parse($window.localStorage.userDetails);
+        }
+    }
+    init();
+    return {
+        login: login,
+        logout: logout,
+        getuserDetails: getuserDetails
+    };
+}]);
